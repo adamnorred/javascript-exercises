4137e004a2d7bc2ee281412efee6e60dac322511
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-10 21:01:21 +0200

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (items, ...args) {
+  for (const arg of args) {
+    if (items.includes(arg)) {
+      const index = items.indexOf(arg);
+      items.splice(index, 1);
+    }
+  }
+  return items;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
