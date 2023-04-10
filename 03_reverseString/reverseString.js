38057897a111acacbdad9e9603142e7b38931f86
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-10 18:40:42 +0200

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (input) {
+  let splitStringToArray = input.split("");
+  let reverseArray = splitStringToArray.reverse();
+  let joinArray = reverseArray.join("");
 
+  return joinArray;
 };
 
 // Do not edit below this line
