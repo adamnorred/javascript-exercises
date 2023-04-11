7f7a712f1fb124f7f012ddea844d742beb89da5d
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-11 10:04:04 +0200

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  const celsius = (fahr - 32) * (5 / 9);
+  const roundNumber = Math.round(celsius * 10) / 10;
+  return roundNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cels) {
+  const fahrenheit = cels * 1.8 + 32;
+  const roundNumber = Math.round(fahrenheit * 10) / 10;
+  return roundNumber;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
