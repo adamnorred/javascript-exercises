20cf67a4fb9b7d9c8a2f189b48d082fdfdd04760
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-25 19:20:18 +0200

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+function reverseString(str) {
+  const splitString = str.split("");
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray;
+}
 
+function removePunctuation(str) {
+  return str.replace(/[^A-Za-z0-9\s]/g, "");
+}
+
+function removeWhitespaces(str) {
+  return str.replace(/\s/g, "");
+}
+
+const palindromes = function (text) {
+  const textLower = text.toLowerCase();
+  const textNoPunctuation = removePunctuation(textLower);
+  const cleanText = removeWhitespaces(textNoPunctuation);
+  const reversedText = reverseString(cleanText);
+
+  return cleanText === reversedText;
 };
 
 // Do not edit below this line
