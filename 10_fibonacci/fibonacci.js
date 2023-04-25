564046d03d8938238631e2fc699fd0363e584ebf
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-25 21:47:13 +0200

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (typeof num === "string") {
+    Number(num);
+  }
+  if (num === "1") {
+    return 1;
+  }
 
+  let first = 0;
+  let second = 1;
+  let third = 0;
+
+  for (let i = 1; i < num; i++) {
+    third = first + second;
+    first = second;
+    second = third;
+  }
+  return third;
 };
 
 // Do not edit below this line
