bcc86475c590b428d2051196c724a6436b43a255
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-26 12:38:12 +0200

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  return array.map(function (item) {
+    return item.title;
+  });
 };
 
 // Do not edit below this line
