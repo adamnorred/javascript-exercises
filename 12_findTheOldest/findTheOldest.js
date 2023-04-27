c89717cbe323a80c9fd5f086330d8715cc176d4e
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-04-27 14:19:09 +0200

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+  return obj.reduce(function (total, currentValue) {
+    const date = new Date();
+    let currentYear = date.getFullYear();
 
+    if (total["yearOfDeath"] === undefined) {
+      total["yearOfDeath"] = currentYear;
+    }
+    if (currentValue["yearOfDeath"] === undefined) {
+      currentValue["yearOfDeath"] = currentYear;
+    }
+
+    if (
+      total.yearOfDeath - total.yearOfBirth >
+      currentValue.yearOfDeath - currentValue.yearOfBirth
+    ) {
+      return total;
+    }
+    return currentValue;
+  });
 };
 
 // Do not edit below this line
